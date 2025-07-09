6bc7abc3fb58fcf352984ecee9307f3e36e82e9f
saumittras <saumittra.eee@gmail.com>
2025-07-10 01:29:58 +0600

    added server related error handleing

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,11 @@
-import cors from "cors";
 import express, { Request, Response } from "express";
-
+/* eslint-disable no-console */
 const app = express();
-app.use(express.json());
-app.use(cors());
 
-app.use("/api/v1");
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
-    message: "Welcome to Toure Management System Backend",
+    message: "Welcome to Finix Elplore Backend",
   });
 });
-// app.use(globalError);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,82 @@
-import express from "express";
+/* eslint-disable no-console */
 import { Server } from "http";
 import mongoose from "mongoose";
+import app from "./app";
 
 let server: Server;
-const app = express();
-
-const startSever = async () => {
+const startServer = async () => {
   try {
     await mongoose.connect(
       `mongodb+srv://saumittra_DB:Saumittra12@tsoftelectronics.l4wjuvt.mongodb.net/tour_DB?retryWrites=true&w=majority&appName=TSoftElectronics`
     );
-    console.log(`Connected to Mongo DB Server`);
-    app.listen(5000, () => {
-      console.log("Server is listening to port 5000");
+
+    console.log("Connected to DB!!");
+
+    server = app.listen(5000, () => {
+      console.log(`Server is listening to port 5000`);
     });
   } catch (error) {
     console.log(error);
   }
 };
-startSever();
+
+startServer();
+
+/**
+ * unhandle rejection error
+ * uncaught rejection error
+ * signal termination sigterm
+ */
+
+// unhandle rejection error
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection detected... Server shutting down", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+//  * unhandle rejection error
+// Promise.reject(new Error("I forget to catch this promise"));
+
+// Error No 2: uncaught rejection error
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception detected... Server shutting down", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+//**uncaught rejection error
+// throw new Error("I forgot to handle this local error");
+
+// * signal termination sigterm
+process.on("SIGTERM", () => {
+  console.log("SIGTERM Signal received... Server shutting down");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("SIGINT", () => {
+  console.log("SIGINT Signal received... Server shutting down");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
